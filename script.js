81c875739192a31bee3a784019f58d2d843b12c9
Nasif Fuad <nasifuad007@gmail.com>
2024-09-26 15:48:48 +0600

    Home page data update

--- script.js
+++ script.js
@@ -3,31 +3,62 @@ const poke_name = document.getElementById("poke-name");
 const poke_name_one = document.getElementById("poke-name-one");
 const pokemon_img = document.getElementById("pokemon-img");
 const pokemon_img_one = document.getElementById("pokemon-img-one");
-// const search = document.getElementById("Search");
-// const search_one = document.getElementById("Search-one");
+const box1 = document.getElementById("img-box1");
+const box2 = document.getElementById("img-box2");
 const battle = document.getElementById("battle");
-async function getPokemon(name) {
-  const res = await fetch(`${poke_url + name}`);
+
+async function getPokemon(url) {
+  const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
-  console.log(data.sprites);
-  const types = data.types.map((typeInfo) => {
-    typeInfo.type.name;
-  });
-  console.log(types);
-  return data.sprites.front_shiny;
+  return data;
 }
 async function showPokemon(name) {
-  const font_image = await getPokemon(name);
-  console.log(font_image);
-  pokemon_img.style.display = "block";
-  pokemon_img.src = font_image;
+  const url = poke_url + name;
+  const data = await getPokemon(url);
+  console.log(data);
+  const PokemonCard = document.createElement("div");
+  PokemonCard.innerHTML = `
+   <div class="img-box2">
+            <img
+              src=""
+              width="220px"
+              alt="img"
+              id="pokemon-img"
+            />
+            <div class="power-stats-one">
+              <div class="power">Atk: 78/100</div>
+              <div class="def">Atk: 32/100</div>
+              <div class="type">Type:Fire</div>
+              <div class="stronger">Strong: Grass</div>
+              <div class="weaker">Weak : Rock</div>
+            </div>
+          </div>
+  `;
+  verses.appendChild(PokemonCard);
 }
 async function showPokemon_one(name) {
-  const font_image = await getPokemon(name);
-  console.log(font_image);
-  pokemon_img_one.style.display = "block";
-  pokemon_img_one.src = font_image;
+  const url = poke_url + name;
+  const data = await getPokemon(url);
+  console.log(data);
+  const PokemonCard = document.createElement("div");
+  PokemonCard.innerHTML = `
+   <div class="img-box2">
+            <img
+              src=""
+              width="220px"
+              alt="img"
+              id="pokemon-img"
+            />
+            <div class="power-stats-one">
+              <div class="power">Atk: 78/100</div>
+              <div class="def">Atk: 32/100</div>
+              <div class="type">Type:Fire</div>
+              <div class="stronger">Strong: Grass</div>
+              <div class="weaker">Weak : Rock</div>
+            </div>
+          </div>
+  `;
+  verses.appendChild(PokemonCard);
 }
 
 battle.addEventListener("click", () => {

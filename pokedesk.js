7e73862b933932477a9e02dc78f8285b967ffed8
Nasif Fuad <nasifuad007@gmail.com>
2024-09-26 07:09:23 +0600

    grabbed data from API

--- pokedesk.js
+++ pokedesk.js
@@ -8,21 +8,25 @@ async function getPokemon(url) {
   // console.log(data);
   return data;
 }
-
-async function showPokemon(url) {
-  let name = "pikachu";
-  url = url + name;
-  console.log(url);
-  const data = await getPokemon(url);
-  // console.log(data.results[0].name);
-  // const pokearray = data.results;
-  // console.log(pokearray);
-
-  // pokearray.forEach((pokemon) => {
-  //   const poke_name = pokemon.name;
-  //   console.log(poke_name);
-  // });
-  console.log(data);
+for (let i = 1; i <= 20; i++) {
+  async function showPokemon(url) {
+    // let name = "pikachu";
+    let id = i;
+    url = url + id;
+    const data = await getPokemon(url);
+    console.log(data);
+    const type = data.types[0].type.name;
+    const name = data.name;
+    const img_link = data.sprites.front_default;
+    const ability_arr = [];
+    const base_power = data.base_experience;
+    console.log(base_power);
+    for (let i = 0; i < data.abilities.length; i++) {
+      ability_arr.push(data.abilities[i].ability.name);
+    }
+    console.log(ability_arr);
+    console.log(name);
+    console.log(data.types[0].type.name);
+  }
+  showPokemon(poke_url);
 }
-
-showPokemon(poke_url);
